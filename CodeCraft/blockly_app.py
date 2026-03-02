51d2210ffe226d41380583a05f7377b1566c4dd4
HINATA-03h <24e1803h@shinshu-u.ac.jp>
2026-03-02 22:53:30 +0900

    Update blockly_app.py

--- CodeCraft/blockly_app.py
+++ CodeCraft/blockly_app.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="wide")
-
-with open("CodeCraft/index.html", encoding="utf-8") as f:
-    html = f.read()
-
-components.html(html, height=900, scrolling=False)import streamlit as st
-import streamlit.components.v1 as components
-
 # ✅ Streamlitの余白を完全削除
 st.set_page_config(layout="wide")
 
